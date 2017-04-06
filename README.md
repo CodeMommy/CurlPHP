355a59505c4a789247b4ec1e617613f07aea0ed6
Candison <kandisheng@163.com>
2017-04-06 17:08:12 +0800

    0.0.1

---
+++ README.md
@@ -0,0 +1,23 @@
+# CodeMommy CurlPHP
+
+[![License](https://poser.pugx.org/CodeMommy/CurlPHP/license?format=flat-square)](LICENSE)
+[![Download](https://poser.pugx.org/CodeMommy/CurlPHP/downloads?format=flat-square)](https://packagist.org/packages/CodeMommy/CurlPHP)
+[![Stable](https://poser.pugx.org/CodeMommy/CurlPHP/version?format=flat-square)](https://packagist.org/packages/CodeMommy/CurlPHP)
+[![Unstable](https://poser.pugx.org/CodeMommy/CurlPHP/v/unstable?format=flat-square)](https://packagist.org/packages/CodeMommy/CurlPHP)
+[![composer.lock Available](https://poser.pugx.org/CodeMommy/CurlPHP/composerlock?format=flat-square)](https://packagist.org/packages/CodeMommy/CurlPHP)
+
+
+> CodeMommy CurlPHP is a request helper for web development.
+
+Visit [CodeMommy Website](http://www.codemommy.com) or [Packagist](https://packagist.org/packages/CodeMommy/CurlPHP) to get more information.
+
+## Authors
+
+| Name | Identity | Social |
+| :--- | :------- | :----- |
+| Candison November | Creator  | [Website](http://www.kandisheng.com) - [GitHub](https://github.com/KanDisheng) |
+
+## More
+
+- [Feedback](https://github.com/CodeMommy/CurlPHP/issues)
+- [About CodeMommy](https://github.com/CodeMommy/CodeMommy)
---
+++ source/Curl.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * CodeMommy CurlPHP
+ * @author  Candison November <www.kandisheng.com>
+ */
+
+namespace CodeMommy\CurlPHP;
+
+/**
+ * Class Curl
+ * @package CodeMommy\CurlPHP
+ */
+class Curl
+{
+    public static function get($url)
+    {
+        $return = file_get_contents($url);
+        if (!$return) {
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+            $return = curl_exec($curl);
+            curl_close($curl);
+        }
+        return $return;
+    }
+
+    public static function post($url, $data)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_NOBODY, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        $return = curl_exec($curl);
+        curl_close($curl);
+        return $return;
+    }
+
+    public static function postJSON($url, $data = null)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        if (!empty($data)) {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $return = curl_exec($curl);
+        curl_close($curl);
+        return $return;
+    }
+}
